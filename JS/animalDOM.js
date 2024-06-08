343ed32469c58447e24a6bd8895365fef012ebf2
bellamoss77 <mmf295@icloud.com>
2024-06-07 23:33:59 -0400

    fix two images, start animal gallery

---
+++ JS/animalDOM.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    galleryContainer = document.getElementById('container');
+
+    const images = [{
+        src: 'https://github.com/bellamoss77/portfolio-page/blob/main/ANIMALS/Apollo_1.png?raw=true',
+        alt: 'dog',
+        caption: 'Apollo Smiles - Central Virginia, Spring 2024'
+        }, {
+        src: 'https://github.com/bellamoss77/portfolio-page/blob/main/ANIMALS/Hikes-Late-May24_13.png?raw=true',
+        alt: 'geese',
+        caption: 'Fine Formation - Central Virginia, Late May 2024'
+        }, {
+        src: 'https://github.com/bellamoss77/portfolio-page/blob/main/ANIMALS/IMG_0385%20copy.jpg?raw=true',
+        alt: 'dog',
+        caption: 'Thoughtful Athena - Central Virgina, Late Winter 2024'
+        }, {
+        src: 'https://github.com/bellamoss77/portfolio-page/blob/main/ANIMALS/IMG_0639.JPG?raw=true',
+        alt: 'dog',
+        caption: 'Stoic - Central Virginia, Spring 2024'
+        }, {
+        src: ''
+        }
+    ]
+})
